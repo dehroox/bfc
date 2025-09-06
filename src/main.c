1b53958fe8bd9d17afe7268a4171762a200e807b
dehroox <dehrexd@gmail.com>
2025-09-06 17:13:12 +0800

    help

--- src/main.c
+++ src/main.c
@@ -11,11 +11,24 @@
 #define OPEN_BRACKET  "["
 #define CLOSE_BRACKET "]"
 
+static inline void help(void);
+
 int main(int argc, char **argv) {
 
-	puts("Hello World!");
+	help();
 
 	return EXIT_SUCCESS;
 	(void)argc;
 	(void)argv;
+}
+
+static inline void help(void) {
+	(void)puts("BFC - Brainfuck Compiler\n"
+			   "    Usage:\n"
+			   "      `bfc [bf file] -o [asm output]`\n\n"
+			   "    Options:\n"
+			   "      -o  - Sets output file, by default [inputname].bf.o.s\n"
+			   "      -a  - Sets architecture output, by default [native]\n"
+			   "	  -np - Disables preprocessing\n"
+			   "      -no - Disables optimization");
 }
