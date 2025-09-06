c5e194c8d57be2206fc498a601456620b0ef7a6f
dehroox <dehrexd@gmail.com>
2025-09-06 17:02:36 +0800

    instructions

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Instruction Set */
+#define MOVE_LEFT	  "<"
+#define MOVE_RIGHT	  ">"
+#define INCREMENT	  "+"
+#define DECREMENT	  "-"
+#define OUTPUT		  "."
+#define INPUT		  ","
+#define OPEN_BRACKET  "["
+#define CLOSE_BRACKET "]"
+
 int main(int argc, char **argv) {
 
 	puts("Hello World!");
